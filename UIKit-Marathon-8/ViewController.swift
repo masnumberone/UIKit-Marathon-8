e1eaa3bfd80c1a1e807d1b91644bbd48ce26514f
masnumberone <m.myxortov2011@gmail.com>
2023-05-23 21:41:36 +0300

    done technical task

--- UIKit-Marathon-8/ViewController.swift
+++ UIKit-Marathon-8/ViewController.swift
@@ -9,9 +9,34 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var imageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        title = "Avatar"
+        view.backgroundColor = .systemBackground
+        
+        let scrollView = UIScrollView(frame: .init(x: 0, y: 0, width: view.frame.width, height: view.frame.height))
+        scrollView.contentSize = .init(width: scrollView.frame.width, height: 1500)
+        view.addSubview(scrollView)
+        
+        navigationController?.navigationBar.prefersLargeTitles = true
+        
+        imageView = UIImageView(image: UIImage(systemName: "person.crop.circle.fill"))
+        imageView.frame = .init(x: (navigationController?.navigationBar.subviews[1].frame.width)! - 36 - 16,
+                                y: -(navigationController?.navigationBar.subviews[1].frame.height)!,
+                                width: 36,
+                                height: 36)
+        scrollView.addSubview(imageView)
+
+        let additionalView = UIView(frame: .init(x: 0,
+                                                 y: 0,
+                                                 width: (navigationController?.navigationBar.subviews[1].frame.width)!,
+                                                 height: 2 * (navigationController?.navigationBar.subviews[1].frame.height)!))
+        
+        additionalView.backgroundColor = .white
+        view.addSubview(additionalView)
     }
 
 
